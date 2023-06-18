9f7e2a99f94364ee30bd03ced31f182a287852d3
aaronlinley <linley.aaron@gmail.com>
2023-06-18 19:34:59 +0100

    fix: escape apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default async function Home() {
   return (
     <main className={`flex min-h-screen min-w-screen flex-col items-center justify-center p-24 relative bg-blue-100`}>
       <div className="container">
-        <h1 className={`text-blue-900 text-2xl ${merriweather.className} mb-4`}>Hi there, I'm Aaron.</h1>
+        <h1 className={`text-blue-900 text-2xl ${merriweather.className} mb-4`}>Hi there, I&apos;m Aaron.</h1>
 
-        <p className="mb-4">I'm a software engineer based in Yorkshire, currently enjoying working with <strong>React</strong> (via Next.js) & <strong>Node</strong>.</p>
+        <p className="mb-4">I&apos;m a software engineer based in Yorkshire, currently enjoying working with <strong>React</strong> (via Next.js) & <strong>Node</strong>.</p>
 
         <p className="mb-4">My day-to-day work consists of React, Node, Symfony and REST API design (OOP PHP).</p>
 
