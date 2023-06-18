a45a41ff7b932037b46d4ed414ad78a952471f02
aaronlinley <linley.aaron@gmail.com>
2023-06-18 21:06:01 +0100

    feat: better mobile experience

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const merriweather = Merriweather({ weight: ["700"], subsets: ["latin"] })
 
 export default async function Home() {
   return (
-    <main className={`flex min-h-screen min-w-screen flex-col items-center justify-center p-24 relative bg-blue-100`}>
+    <main className={`flex min-h-screen min-w-screen flex-col items-center justify-center p-12 md:p-24 relative bg-blue-100`}>
       <div className="container">
         <h1 className={`text-blue-900 text-2xl ${merriweather.className} mb-4`}>Hi there, I&apos;m Aaron.</h1>
 
@@ -15,19 +15,19 @@ export default async function Home() {
 
         <p className="mb-4">You can find me around the internet:</p>
 
-        <ul className="flex flex-col md:flex-row md:items-center gap-6">
+        <ul className="flex items-center gap-6">
           <li>
-            <a href="https://github.com/aaronlinley" target="_blank" rel="noopener" className="flex items-center py-2 px-3 border border-slate-700 gap-2 rounded">
+            <a href="https://github.com/aaronlinley" target="_blank" rel="noopener" className="py-2 px-3 border border-slate-700 rounded">
               <span>Github</span>
             </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/aaron-linley-6a742984/" target="_blank" rel="noopener" className="flex items-center py-2 px-3 border border-slate-700 gap-2 rounded">
+            <a href="https://www.linkedin.com/in/aaron-linley-6a742984/" target="_blank" rel="noopener" className="py-2 px-3 border border-slate-700 rounded">
               LinkedIn
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/pigboypaints" target="_blank" rel="noopener" className="flex items-center py-2 px-3 border border-slate-700 gap-2 rounded">
+            <a href="https://twitter.com/pigboypaints" target="_blank" rel="noopener" className="py-2 px-3 border border-slate-700 rounded">
               Twitter
             </a>
           </li>
